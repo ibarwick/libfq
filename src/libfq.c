06896846aa1f12a97111934eaba8e940dc7fd108
Ian Barwick <barwick@gmail.com>
2018-11-09 15:50:38 +0900

    Remove pointless initialization of isc_blob_handle variables

--- src/libfq.c
+++ src/libfq.c
@@ -1794,7 +1794,7 @@ _FQexecParams(FBconn *conn,
 
 				case SQL_BLOB:
 				{
-					isc_blob_handle blob_handle = NULL;
+					isc_blob_handle blob_handle;
 					char *ptr = (char *)paramValues[i];
 
 					len = strlen(paramValues[i]);
@@ -3298,7 +3298,7 @@ _FQformatDatum(FBconn *conn, FQresTupleAttDesc *att_desc, XSQLVAR *var)
         {
             ISC_QUAD *blob_id = (ISC_QUAD *)var->sqldata;
 
-            isc_blob_handle blob_handle = NULL;
+            isc_blob_handle blob_handle;
             char blob_segment[BLOB_SEGMENT_LEN];
             unsigned short actual_seg_len;
             ISC_STATUS blob_status;
